e7579f8a7a38fd35e936a86f51ac64402eb024e2
MoeJaber <mouhamadjaber@live.com>
2017-06-08 15:08:12 -0400

    Add project files.

---
+++ Parser/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Parser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.settings = new MetroFramework.Controls.MetroContextMenu(this.components);
+            this.opac = new System.Windows.Forms.ToolStripMenuItem();
+            this.two_five = new System.Windows.Forms.ToolStripMenuItem();
+            this.five_zero = new System.Windows.Forms.ToolStripMenuItem();
+            this.seven_five = new System.Windows.Forms.ToolStripMenuItem();
+            this.hundred = new System.Windows.Forms.ToolStripMenuItem();
+            this.settings.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(23, 58);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(112, 23);
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "Folder Dialog";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(431, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(69, 69);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(23, 89);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Multiline = true;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(501, 71);
+            this.metroTextBox1.TabIndex = 1;
+            this.metroTextBox1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.metroTextBox1.Click += new System.EventHandler(this.metroTextBox1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(23, 29);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(112, 23);
+            this.metroButton2.TabIndex = 2;
+            this.metroButton2.Text = "Keyword File";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(141, 33);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(18, 19);
+            this.metroLabel1.TabIndex = 3;
+            this.metroLabel1.Text = "...";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(141, 62);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(18, 19);
+            this.metroLabel2.TabIndex = 4;
+            this.metroLabel2.Text = "...";
+            this.metroLabel2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.BackColor = System.Drawing.Color.Black;
+            this.metroButton3.Enabled = false;
+            this.metroButton3.ForeColor = System.Drawing.Color.Cornsilk;
+            this.metroButton3.Location = new System.Drawing.Point(23, 166);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(501, 23);
+            this.metroButton3.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroButton3.TabIndex = 5;
+            this.metroButton3.Text = "View Findings";
+            this.metroButton3.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton3.UseCustomBackColor = true;
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.UseStyleColors = true;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // settings
+            // 
+            this.settings.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opac});
+            this.settings.Name = "settings";
+            this.settings.Size = new System.Drawing.Size(116, 26);
+            this.settings.Style = MetroFramework.MetroColorStyle.Purple;
+            this.settings.Text = "Settings";
+            this.settings.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.settings.UseStyleColors = true;
+            // 
+            // opac
+            // 
+            this.opac.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.two_five,
+            this.five_zero,
+            this.seven_five,
+            this.hundred});
+            this.opac.Image = ((System.Drawing.Image)(resources.GetObject("opac.Image")));
+            this.opac.Name = "opac";
+            this.opac.Size = new System.Drawing.Size(115, 22);
+            this.opac.Text = "Opacity";
+            // 
+            // two_five
+            // 
+            this.two_five.Image = ((System.Drawing.Image)(resources.GetObject("two_five.Image")));
+            this.two_five.Name = "two_five";
+            this.two_five.Size = new System.Drawing.Size(102, 22);
+            this.two_five.Text = "25%";
+            this.two_five.Click += new System.EventHandler(this.two_five_Click);
+            // 
+            // five_zero
+            // 
+            this.five_zero.Image = ((System.Drawing.Image)(resources.GetObject("five_zero.Image")));
+            this.five_zero.Name = "five_zero";
+            this.five_zero.Size = new System.Drawing.Size(102, 22);
+            this.five_zero.Text = "50%";
+            this.five_zero.Click += new System.EventHandler(this.five_zero_Click);
+            // 
+            // seven_five
+            // 
+            this.seven_five.Image = ((System.Drawing.Image)(resources.GetObject("seven_five.Image")));
+            this.seven_five.Name = "seven_five";
+            this.seven_five.Size = new System.Drawing.Size(102, 22);
+            this.seven_five.Text = "75%";
+            this.seven_five.Click += new System.EventHandler(this.seven_five_Click);
+            // 
+            // hundred
+            // 
+            this.hundred.Image = ((System.Drawing.Image)(resources.GetObject("hundred.Image")));
+            this.hundred.Name = "hundred";
+            this.hundred.Size = new System.Drawing.Size(102, 22);
+            this.hundred.Text = "100%";
+            this.hundred.Click += new System.EventHandler(this.hundred_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 208);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.metroButton1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0.75D;
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Purple;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.settings.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroContextMenu settings;
+        private System.Windows.Forms.ToolStripMenuItem opac;
+        private System.Windows.Forms.ToolStripMenuItem two_five;
+        private System.Windows.Forms.ToolStripMenuItem five_zero;
+        private System.Windows.Forms.ToolStripMenuItem seven_five;
+        private System.Windows.Forms.ToolStripMenuItem hundred;
+    }
+}
+
---
+++ Parser/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Parser
+{
+    public partial class Form1 : MetroForm
+    {
+        private static List<String> files = new List<string>();
+        private static string[] Keys;
+        private static int key_set = 0;
+        private static int keyCount = 0;
+        private static List<String> PATHS = new List<string>();
+        private static List<String> LINE_MATCH = new List<string>();
+        private static List<String> LINE_NUM = new List<string>();
+        private static List<String> KEYWORD = new List<string>();
+        private static string table = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.ContextMenuStrip = settings;
+            settings.Show();
+        }
+  
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog openFolder = new FolderBrowserDialog();
+            openFolder.ShowNewFolderButton = false;
+            openFolder.Description = "Select Folder to literate through";
+     
+           
+            if (key_set == 1) {
+              
+                if (openFolder.ShowDialog() == DialogResult.OK)
+            {
+                    metroLabel2.Text = openFolder.SelectedPath;
+                    string PATH = openFolder.SelectedPath;
+
+                    try
+                    {                                                                                               
+                        Search(PATH);
+                       // metroButton3.Style = Red;       
+                    }//end try
+
+                catch (Exception ex)
+                {
+                    MetroFramework.MetroMessageBox.Show(this, "Error: Could not read file from disk. Original error: " + ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                }//end catch
+
+                    //foreach(string match in matches)
+                    //{
+                    //    Console.WriteLine(match);
+                    //}
+                    //Console.WriteLine(matches.Count.ToString());
+
+                    PATH = "";
+                }//end if
+            }//end of is key_set
+            else
+            {
+                MetroFramework.MetroMessageBox.Show(this, "Select a KEYWORD File before you continue." , "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }//if key_set is not set
+
+            
+        }//end button
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                metroTextBox1.Text = File.ReadAllText(openFile.FileName);         
+                Keys = File.ReadAllLines(openFile.FileName);
+                key_set = 1;
+                metroLabel1.Text = openFile.FileName;
+                keyCount = Keys.Count();
+            }
+        }
+
+        private void metroTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Search(string PATH){
+
+            string excludedExtensions = "*.zip";
+            //add all folder file paths to array
+            var files = Directory.GetFiles(PATH, "*.*", SearchOption.AllDirectories).Where(s => !excludedExtensions.Contains(Path.GetExtension(s).ToLower()));
+
+            //convert array to List
+            files.ToList();
+
+
+            foreach (string key in Keys)
+            {
+
+                int counter = 1;
+                Console.WriteLine("Files {0}", files.Count());
+                string line;
+
+                //emnumerate through the list 
+                foreach (string path in files)
+                {
+                    //open file for reading
+                    System.IO.StreamReader file = new System.IO.StreamReader(path);
+                    //Console.WriteLine("{0} \n ", path);
+                    while ((line = file.ReadLine()) != null)
+                    {
+
+                        if (line.Contains(key))
+                        {
+                            //matches.Add("File Path:: " + path + "\nMatching Line:: " + line + "\nLine Number:: " + counter.ToString() + "\nKeyword:: " + key + "\n");
+                            PATHS.Add(path);
+                            LINE_MATCH.Add(line);
+                            LINE_NUM.Add(counter.ToString());
+                            KEYWORD.Add(key);
+                        }
+
+                        //Console.WriteLine(line);
+                        counter++;
+
+                    }//end while
+                  
+                    counter = 1;
+                    file.Close();
+                    Console.ReadLine();
+
+                }//end foreach
+            }//end keyword foreach
+            
+
+            using (FileStream fs = new FileStream("MATCHES.htm", FileMode.Create))
+            {
+                using (StreamWriter w = new StreamWriter(fs, Encoding.UTF8))
+                {          
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("<!-- Mouhamad Jaber --!>\n");
+                    sb.Append("<style> \n table tr:nth-child(even){background-color: #eee;} \n table tr:nth-child(odd){background-color: #fff;} \n table th{background-color: black;color: white; border: 1px solid black;}\n table td{border: 1px solid black;} \n table {border: 1px solid black;  border-collapse: collapse;} \n</style>\n\n");
+                    sb.Append("<table> \n\t<tr>\n\t\t<th>KEYWORD</th>\n\t\t<th>FILE PATH</th>\n\t\t<th>MATCH</th>\n\t\t<th>LINE #</th>\n\t</tr>\n\n");
+
+                    for (int x = 0; x < PATHS.Count; x++)                   
+                    {
+                        sb.AppendFormat("\n\t<tr>\n\t\t<td>{0}</td>\n", KEYWORD[x]);
+                        sb.AppendFormat("\t\t<td>{0}</td>\n", PATHS[x]);
+                        sb.AppendFormat("\t\t<td>{0}</td>\n", LINE_MATCH[x]);
+                        sb.AppendFormat("\t\t<td>{0}</td>\n\t</tr>\n", LINE_NUM[x]);                       
+                    }
+                    
+                    sb.Append("</table>");
+                    w.WriteLine(sb);
+
+                    table = fs.Name;
+                    // table = System.Uri.EscapeUriString(table);
+                    
+                    table = Regex.Replace(table, @"\s+", "%20");                   
+                    Console.WriteLine(table);
+                    metroButton3.Enabled = true;
+                    
+                }
+            }
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("chrome.exe", table);
+            }
+            catch (Win32Exception)
+            {
+                Process.Start("IExplore.exe", table);
+            }
+
+                Console.WriteLine(table);
+        }
+
+        private void two_five_Click(object sender, EventArgs e)
+        {
+            MetroForm.ActiveForm.Opacity = .25;
+            Console.WriteLine("25");
+        }
+
+        private void five_zero_Click(object sender, EventArgs e)
+        {
+            MetroForm.ActiveForm.Opacity = .50;
+            Console.WriteLine("50");
+        }
+
+        private void seven_five_Click(object sender, EventArgs e)
+        {
+            MetroForm.ActiveForm.Opacity = .75;
+            Console.WriteLine("75");
+        }
+
+        private void hundred_Click(object sender, EventArgs e)
+        {
+            MetroForm.ActiveForm.Opacity = 1;
+            Console.WriteLine("100");
+        }
+    }//end form
+}//end namespace
